876cd4c2e1038eef6d9e147003bc0ea223b66354
Joonas Tikkanen <joonas.tikkanen@ambientia.fi>
2021-04-28 09:48:34 +0300

    fix script

--- scripts/download_plugins.sh
+++ scripts/download_plugins.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-cd /var/www/html/wp-content/plugins
+PLUGINS_FOLDER=/var/www/html/wp-content/plugins
 for PLUGIN in $1
 do
   echo "DOWNLOADING WORDPRESS PLUGIN "$PLUGIN_NAME
   PLUGIN_NAME=`awk -F"|" '{print $1}' $PLUGIN`
   PLUGIN_VERSION=`awk -F"|" '{print $2}' $PLUGIN`
-  wget https://downloads.wordpress.org/plugin/$PLUGIN_NAME.$PLUGIN_VERSION.zip
-  unzip $PLUGIN_NAME.$PLUGIN_VERSION.zip
-  rm $PLUGIN_NAME.$PLUGIN_VERSION.zip
+  wget https://downloads.wordpress.org/plugin/$PLUGIN_NAME.$PLUGIN_VERSION.zip -O $PLUGINS_FOLDER
+  unzip $PLUGINS_FOLDER/$PLUGIN_NAME.$PLUGIN_VERSION.zip -d $PLUGINS_FOLDER
+  rm $PLUGINS_FOLDER/$PLUGIN_NAME.$PLUGIN_VERSION.zip
   echo "WORDPRESS PLUGIN "$PLUGIN_NAME " INSTALLED"
   echo
 done
